f0f9c7bef0f1b70f28d8f0576a199474706ec0c4
BuildTools <unconfigured@null.spigotmc.org>
2021-08-19 22:23:38 -0400

    initial commit

---
+++ components/MetaTags.tsx
@@ -0,0 +1,31 @@
+
+import Head from 'next/head';
+
+export default function MetaTags({
+	title = 'Monad',
+	description = 'Neesh\'s Personal Website',
+	image = '/favicon.svg',
+	favicon = '/favicon.ico',
+	color = '#0B172A',
+}) {
+	return (
+		<Head>
+			<title>{title}</title>
+
+			<meta name='description' content={description} />
+			<link rel='icon' type='image/png' href={image} />
+			<meta name='image' content={image} />
+			<meta name='theme-color' content={color} />
+
+            <meta name='twitter:card' content='summary' />
+			<meta name='twitter:site' content='@neesh774' />
+			<meta name='twitter:title' content={title} />
+			<meta name='twitter:description' content={description} />
+			<meta name='twitter:image' content={image} />
+
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			<meta property='og:image' content={image} />
+		</Head>
+	);
+}
