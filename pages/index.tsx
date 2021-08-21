6e7547aaf2d4e1bb146944c83d3d2b0dfe78f5a4
Neesh774 <Kanishq0106@gmail.com>
2021-08-21 00:32:36 -0400

    navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import MetaTags from '../components/MetaTags'
+import NavBar from '../components/NavBar'
 const Home: NextPage = () => {
   return (
     <>
       <MetaTags/> 
+      <div>
+        <NavBar/>
+      </div>
     </>
   )
 }
