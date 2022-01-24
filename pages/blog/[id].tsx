5178ecaac2603afb56c325996c2fa53c96f1314f
Neesh774 <Kanishq0106@gmail.com>
2022-01-24 13:57:54 -0500

    blog responsiveness fixes

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -76,7 +76,7 @@ const renderBlock = (block) => {
     case "child_page":
       return <p>{value.title}</p>;
     case "code":
-      return <Code code={value}/>
+      return <Code code={value} />;
     default:
       return `[❌ Unsupported block (${
         type === "unsupported" ? "unsupported by Notion API" : type
@@ -91,12 +91,23 @@ export default function Post({ page, blocks }) {
 
   return (
     <>
-      <MetaTags title={page.properties.Name ? page.properties.Name.title[0].plain_text :  "NO TITLE"}  description={page.properties.Description ? page.properties.Description.rich_text[0].plain_text : "NO DESCRIPTION"}/>
+      <MetaTags
+        title={
+          page.properties.Name
+            ? page.properties.Name.title[0].plain_text
+            : "NO TITLE"
+        }
+        description={
+          page.properties.Description
+            ? page.properties.Description.rich_text[0].plain_text
+            : "NO DESCRIPTION"
+        }
+      />
       <div className="divide-y-2 divide-gray-500 divide-opacity-60 px-5">
         <div className="mx-5">
           <div className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-yellow-700 font-extrabold mt-5 mb-1">
             <Text text={page.properties.Name.title} />
-            <div className="flex space-x-2 mr-2 mt-2">
+            <div className="sm:space-x-2 mr-2 mt-2 flex flex-col sm:flex-row">
               <div className="text-gray-500 font-normal text-sm">
                 Created&nbsp;
                 <Date dateString={page.properties.Created.created_time} />
@@ -104,17 +115,19 @@ export default function Post({ page, blocks }) {
               <div className="text-gray-500 hidden sm:block text-sm font-normal">
                 •
               </div>
-              {page.properties.Tags.multi_select.map((tag) => (
-                <div key={tag.id}>
-                  <div
-                    className={classNames("rounded-md", tagColor(tag.color))}
-                  >
-                    <div className="px-2 py-1 text-xs text-black font-normal">
-                      {tag.name}
+              <div className="flex flex-col sm:flex-row sm:space-x-1 space-y-1 sm:space-y-0">
+                {page.properties.Tags.multi_select.map((tag) => (
+                  <div key={tag.id} className="flex">
+                    <div
+                      className={classNames("rounded-md", tagColor(tag.color))}
+                    >
+                      <div className="px-2 py-1 text-xs text-black font-normal">
+                        {tag.name}
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
           <div className="my-1">
@@ -125,7 +138,10 @@ export default function Post({ page, blocks }) {
         </div>
         <div className="py-4">
           {blocks.map((block) => (
-            <div key={block.id} className="pt-2 lg:mx-72 sm:mx-36 mx-4">
+            <div
+              key={block.id}
+              className="my-12 mx-auto max-w-layout-max xl:max-w-2xl lg:max-w-xl md:max-w-lg sm:max-w-md xs:max-w-sm"
+            >
               <Fragment>{renderBlock(block)}</Fragment>
             </div>
           ))}
