e61e6d417df5cabd77b4747b13f297ec2f62fcfc
Neesh774 <Kanishq0106@gmail.com>
2022-09-20 08:25:17 -0400

    woo responsive

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -89,6 +89,36 @@ const Projects = ({
       },
     },
   };
+
+  const techParentVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.05,
+        delayChildren: 0.8,
+      },
+    },
+  };
+
+  const techVariants = {
+    hidden: {
+      opacity: 0,
+      translateY: -60,
+    },
+    show: {
+      opacity: 1,
+      translateY: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.7,
+        velocity: 30,
+      },
+    },
+  };
+
   return (
     <motion.div
       initial="hidden"
@@ -140,9 +170,9 @@ const Projects = ({
               key={i}
               initial="hidden"
               whileInView="show"
-              viewport={{ once: true, amount: "some" }}
+              viewport={{ once: true, amount: "all" }}
               variants={projectVariants}
-              className={`flex mb-32 relative ${
+              className={`flex mb-20 md:mb-44 relative ${
                 i % 2 == 0 ? "flex-row" : "flex-row-reverse"
               }`}
             >
@@ -155,19 +185,69 @@ const Projects = ({
                   <span className="font-mono text-highlight text-xs">
                     Featured
                   </span>
-                  <h2 className="font-text font-bold text-3xl text-secondary-100">
-                    <a href={link}>
-                      {project.properties.Name.type == "title" && (
-                        <RichText text={project.properties.Name.title} />
-                      )}
-                    </a>
-                  </h2>
+                  <div
+                    className={`flex md:items-center md:justify-center gap-2 ${
+                      i % 2 == 0
+                        ? "flex-col md:flex-row items-start"
+                        : "flex-col md:flex-row-reverse items-end"
+                    }`}
+                  >
+                    <h2
+                      className={`font-text font-bold text-3xl text-secondary-100 ${
+                        i % 2 == 0 ? "mr-4" : "ml-4"
+                      }`}
+                    >
+                      <a href={link}>
+                        {project.properties.Name.type == "title" && (
+                          <RichText text={project.properties.Name.title} />
+                        )}
+                      </a>
+                    </h2>
+                    <motion.div
+                      variants={techParentVariants}
+                      initial="hidden"
+                      whileInView="show"
+                      viewport={{ amount: 0.7, once: true }}
+                      className={`flex md:flex-row flex-wrap gap-2 ${
+                        i % 2 == 0 ? "justify-start" : "justify-end"
+                      }`}
+                    >
+                      {project.properties.Technologies.type == "multi_select" &&
+                        project.properties.Technologies.multi_select.map(
+                          (tech, i) => (
+                            <motion.span
+                              key={i}
+                              variants={techVariants}
+                              className="text-highlight font-body font-medium text-xs bg-background-700 shadow-lg px-2 py-1 rounded-[.250rem] z-10"
+                            >
+                              {tech.name}
+                            </motion.span>
+                          )
+                        )}
+                    </motion.div>
+                  </div>
                 </div>
-                <p className="bg-background-700 shadow-lg text-text-300 font-body text-sm font-[500] px-6 py-4 min-h-fit w-3/5 rounded-[.250rem] z-10">
-                  {project.properties.Description.type == "rich_text" && (
-                    <RichText text={project.properties.Description.rich_text} />
-                  )}
-                </p>
+                <div className="flex flex-col justify-center items-center md:contents">
+                  <a href={link}>
+                    <motion.img
+                      className="w-5/6 sm:w-2/3 mx-auto relative sm:top-8 object-cover md:hidden"
+                      variants={imageVariants}
+                      initial="hidden"
+                      viewport={{ once: true, amount: 0.4 }}
+                      whileInView="show"
+                      whileHover="hover"
+                      src={imageUrl}
+                      alt={projectTitle}
+                    />
+                  </a>
+                  <p className="bg-background-700 shadow-lg text-text-300 font-body text-sm font-[500] px-6 py-4 min-h-fit sm:w-4/5 md:w-5/6 rounded-[.250rem] z-10">
+                    {project.properties.Description.type == "rich_text" && (
+                      <RichText
+                        text={project.properties.Description.rich_text}
+                      />
+                    )}
+                  </p>
+                </div>
                 <div className="flex flex-row gap-3">
                   {githubLink && (
                     <IconButton onClick={() => router.push(githubLink)}>
@@ -183,8 +263,10 @@ const Projects = ({
               </div>
               <a href={link}>
                 <motion.img
-                  className={`absolute w-1/3 object-cover top-32 ${
-                    i % 2 == 0 ? "left-1/4" : "right-1/4"
+                  className={`absolute w-7/12 lg:w-1/2 object-cover top-40 lg:top-32 hidden md:block ${
+                    i % 2 == 0
+                      ? "lg:left-1/4 left-1/3"
+                      : "lg:right-1/4 right-1/3"
                   }`}
                   variants={imageVariants}
                   initial="hidden"
